418e3b6b2665ff45332dc07dc2ebc2815c88012c
Ellen Cooper <ellencooper74@gmail.com>
2017-08-11 13:31:34 -0700

    adds session create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
   def new
-    
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash[:notice] = "Incorrect email or password. Please try again."
+      redirect_to login_path
+    end
   end
 end
