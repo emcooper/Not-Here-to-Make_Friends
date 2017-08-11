1a98ad570faae656d28924b0b694aced843e25d6
Ellen Cooper <ellencooper74@gmail.com>
2017-08-11 14:25:44 -0700

    adds link and route for new account sign up

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,21 @@
+RSpec.feature "User can sign up for account" do
+  context "with valid username and password" do
+    it "they are able to sign up and are redirected to profile" do
+      name = "Ellen"
+      email = "ellen@gmail.com"
+      password = "password123"
+
+      visit login_path
+      click_on "New User? Create an Account!"
+      fill_in "user_name", with: name
+      fill_in "user_email", with: email
+      fill_in "user_password", with: password
+      fill_in "user_password_confirmation", with: password
+      click_on "Sign Up!"
+
+      expect(current_path).to eq(user_path(User.last))
+      expect(page).to have_content("You've successfully created an account!")
+      expect(page).to have_content("Hi Ellen")
+    end
+  end
+end
