27f2d504e6a293b80e2009f5bc9d933996f58925
Ellen Cooper <ellencooper74@gmail.com>
2017-08-11 14:10:03 -0700

    adds additional sad path test for user login

--- spec/features/user_logs_in_and_out_spec.rb
+++ spec/features/user_logs_in_and_out_spec.rb
@@ -35,4 +35,20 @@ RSpec.feature "User logs in and out" do
       expect(page).to have_content("Incorrect email or password. Please try again.")
     end
   end
+  context "with invalid email" do
+    scenario "they are redirected to login page and shown error message" do
+      email = "user@gmail.com"
+      password = "user123"
+      user = create(:user, email: email, password: password)
+
+      visit login_path
+      fill_in "session_email", with: "wrongemail"
+      fill_in "session_password", with: password
+      click_on "Log In"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to_not have_content(user.name)
+      expect(page).to have_content("Incorrect email or password. Please try again.")
+    end
+  end
 end
