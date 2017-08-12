ad5152393b003c5f41a9bf20bdf53c37456d9d6c
Ellen Cooper <ellencooper74@gmail.com>
2017-08-11 22:17:57 -0700

    added active and retired to factory team names

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
   factory :team do
-    sequence :name {|n| "Team #{n}"}
+    sequence :name {|n| "Active Team #{n}"}
     league
   end
 
   trait :in_retired_league do
     after(:create) do |team|
+      team.name.gsub!("Active", "Retired")
       team.league = create(:league, :in_retired_season)
     end
   end
--- spec/features/authenticated_user_visits_profile_page_spec.rb
+++ spec/features/authenticated_user_visits_profile_page_spec.rb
@@ -1,5 +1,14 @@
 RSpec.feature "Authenticated user visits their profile page" do
   it "sees links to active leagues only" do
     user = create(:user, :with_2_active_and_2_retired_teams)
+    active_teams = Team.where('name LIKE ?','%Active%')
+    retired_teams = Team.where('name LIKE ?','%Retired%')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+
+    expect(page).to have_link(user.teams[0])
+    expect(page).to have_link(user.teams[0])
+    expect(page).to have_link(user.teams[0])
   end
 end
