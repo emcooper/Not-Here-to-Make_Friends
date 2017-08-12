07035bf0cfac016f8c93d41b826ed05736729ce6
Ellen Cooper <ellencooper74@gmail.com>
2017-08-11 22:20:24 -0700

    finishes test for user seeing team name links on profile"

--- spec/features/authenticated_user_visits_profile_page_spec.rb
+++ spec/features/authenticated_user_visits_profile_page_spec.rb
@@ -3,12 +3,14 @@ RSpec.feature "Authenticated user visits their profile page" do
     user = create(:user, :with_2_active_and_2_retired_teams)
     active_teams = Team.where('name LIKE ?','%Active%')
     retired_teams = Team.where('name LIKE ?','%Retired%')
+    
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit user_path(user)
 
-    expect(page).to have_link(user.teams[0])
-    expect(page).to have_link(user.teams[0])
-    expect(page).to have_link(user.teams[0])
+    expect(page).to have_link(active_teams[0].name)
+    expect(page).to have_link(active_teams[1].name)
+    expect(page).to_not have_content(retired.teams[0].name)
+    expect(page).to_not have_content(retired_teams[1].name)
   end
 end
