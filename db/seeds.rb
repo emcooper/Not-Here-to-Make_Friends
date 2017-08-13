d20d235bb7372f4df4c9810af6ad3e3bec916055
Ellen Cooper <ellencooper74@gmail.com>
2017-08-12 22:05:13 -0700

    added basic seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
+
+user = User.create(name: "User", email: "user@gmail.com", password: "password", password_confirmation: "password")
+
+bachelor = SeasonType.create("Bachelor")
+nick_season = Season.create(first_name: "Nick", last_name: "Viall", season_number: 21, season_type: bachelor, status: "active")
+league_1 = League.create(name: "League 1", season: nick_season)
+league_2 = League.create(name: "League 2", season: nick_season)
+team_1a = Team.create(name: "Team 1a", league: league_1)
+team_2a = Team.create(name: "Team 2a", league: league_2)
+user.teams << [team_1a, team_2a]
