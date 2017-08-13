bcf87baf6b466457010cf01834630d3d5dfd3cc0
Ellen Cooper <ellencooper74@gmail.com>
2017-08-13 14:48:37 -0700

    reworked factories to use season traits instead of user traits

--- spec/features/authenticated_user_visits_profile_page_spec.rb
+++ spec/features/authenticated_user_visits_profile_page_spec.rb
@@ -1,16 +1,18 @@
 RSpec.feature "Authenticated user visits their profile page" do
   it "sees links to active leagues only" do
-    user = create(:user, :with_2_active_and_2_retired_teams)
-    active_leagues = League.where('name LIKE ?','%Active%')
-    retired_leagues = League.where('name LIKE ?','%Retired%')
+    user = create(:user)
+    active_season = create(:season, :active_with_existing_users)
+    retired_season = create(:season, :retired_with_existing_users)
+    users_active_leagues = active_season.leagues
+    users_retired_leagues = retired_season.leagues
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit user_path(user)
-    
-    expect(page).to have_button(active_leagues[0].name)
-    expect(page).to have_button(active_leagues[1].name)
-    expect(page).to_not have_content(retired_leagues[0].name)
-    expect(page).to_not have_content(retired_leagues[1].name)
+
+    expect(page).to have_button(users_active_leagues[0].name)
+    expect(page).to have_button(users_active_leagues[1].name)
+    expect(page).to_not have_content(users_retired_leagues[0].name)
+    expect(page).to_not have_content(users_retired_leagues[1].name)
   end
 end
