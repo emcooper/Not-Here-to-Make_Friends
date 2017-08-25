32078b3de5980e49dc9b17cee45ef555e1475ad4
Ellen Cooper <ellencooper74@gmail.com>
2017-08-24 21:06:06 -0600

    added league show route to serve as user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'leagues#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users, only: [:show, :new, :create]
+  resources :leagues, only: [:show]
 end
