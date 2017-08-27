ce3c553bb5d63738f8e487b93e724333203c1ae0
Ellen Cooper <ellencooper74@gmail.com>
2017-08-26 23:02:54 -0600

    adds team summary table to user profile

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,6 +1,5 @@
 class LeaguesController < ApplicationController
   def show
     @league = (League.find(params[:id]) if params[:id]) || current_user.leagues.first
-    render :partial => "users/user_profile_navbar"
   end
 end
--- spec/factories/contestants.rb
+++ spec/factories/contestants.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :contestant do
     name "MyString"
-    headshot "MyString"
+    headshot "http://static5.businessinsider.com/image/587e62e8dd0895e1148b4c30-1392/screen%20shot%202017-01-17%20at%20124437%20pm.png"
     bio "MyString"
-    season 
+    season
   end
 end
