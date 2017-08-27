7de0c163d97f8d5f8c1a919571351a1a8d72345b
Ellen Cooper <ellencooper74@gmail.com>
2017-08-27 16:03:52 -0600

    adds current_season helper method; modifies navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :current_admin?, :require_login
+  helper_method :current_user, :current_admin?, :require_login, :current_season
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_season
+    Season.find_by(status: "active")
   end
 
   def current_admin?
