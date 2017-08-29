6cde636aeb9a05b4ede632cc418270d363858eec
Ellen Cooper <ellencooper74@gmail.com>
2017-08-28 21:14:48 -0600

    adds current_week to application controller, and links in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :current_admin?, :require_login, :current_season
+  helper_method :current_user,
+                :current_admin?,
+                :require_login,
+                :current_season,
+                :current_week
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -8,6 +12,10 @@ class ApplicationController < ActionController::Base
 
   def current_season
     Season.find_by(status: "active")
+  end
+
+  def current_week
+    Week.current_week
   end
 
   def current_admin?
