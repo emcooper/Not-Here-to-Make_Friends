5d9b8b5b168310b3a8fe9d5c8e8b68175f2d999c
Ellen Cooper <ellencooper74@gmail.com>
2017-09-03 10:02:38 -0600

    adds basic styling for weekly show page

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,4 +2,8 @@ class Action < ApplicationRecord
   belongs_to :contestant
   belongs_to :play
   belongs_to :week
+
+  def points
+    play.point_value * count
+  end
 end
--- spec/models/action_spec.rb
+++ spec/models/action_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Action, type: :model do
     it {should belong_to(:play)}
     it {should belong_to(:week)}
   end
+
+  describe "instance methods" do
+    it "#points returns total points of action" do
+      play = create(:play, point_value: 10)
+      action_1 = create(:action, count: 3, play: play)
+      action_2 = create(:action, count: 5)
+
+      expect(action_1.points).to eq(30)
+      expect(action_2.points).to eq(10)
+    end
+  end
 end
