be0a01a5e117b2d3af25df3e381b0294d35b6b8c
Ellen Cooper <ellencooper74@gmail.com>
2017-09-10 15:38:01 -0600

    added VCR

--- app/presenters/contestant_show_presenter.rb
+++ app/presenters/contestant_show_presenter.rb
@@ -4,4 +4,8 @@ class ContestantShowPresenter
   def initialize(company_id)
     @contestant = Contestant.find(company_id)
   end
+
+  def twitter_data
+    TwitterService.tweets(@contestant)
+  end
 end
