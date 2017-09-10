608886bb3a66d544da0037a563cd59051339ac3b
Ellen Cooper <ellencooper74@gmail.com>
2017-09-10 16:02:03 -0600

    adds twitter embedded timeline to contestant show (how is it that easy twitter is my bff)

---
+++ spec/services/twitter_service_spec.rb
@@ -0,0 +1,8 @@
+describe TwitterService do
+  before :each do
+    contestant = create(:contestant, twitter_handle: "@ln")
+    @twitter_service = TwitterService.new(contestant)
+  end
+
+  it "num_followers returns the number of followers" do
+    VCR.use_cassette("services/num_followers") do
