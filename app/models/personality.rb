aa44863bbc64d8ed33dae6d95d203d014ab8b8b2
Ellen Cooper <ellencooper74@gmail.com>
2017-09-12 08:26:21 -0600

    adds attr_reader for personality traits

--- app/models/personality.rb
+++ app/models/personality.rb
@@ -1,31 +1,46 @@
 class Personality
   attr_reader :adventurousness,
+              :artistic,
+              :emotionality,
+              :imagination,
+              :authority_challenging,
+              :cautiousness,
+              :self_discipline,
+              :activity_level,
+              :assertiveness,
+              :cheerfulness,
+              :excitement_seeking,
+              :friendliness,
+              :altruism,
+              :modesty,
+              :morality,
+              :anger,
+              :anxiety,
               :melancholy
 
-
   def initialize(data)
-    @adventurousness = data[:Adventurousness]
-    @artistic = data[:Artistic]
-    @emotionality = data[:Emotionality]
-    @imagination = data[:Imagination]
-    @authority_challenging = data[:Authority_challenging]
-    @cautiousness = data[:Cautiousness]
-    @self_discipline = data[:Self_discipline]
-    @activity_level = data[:Activity_level]
-    @assertiveness = data[:Assertiveness]
-    @cheerfulness = data[:Cheerfulness]
-    @excitement_seeking = data[:Excitement_seeking]
-    @friendliness = data[:Friendliness]
-    @altruism = data[:Altruism]
-    @modesty = data[:Modesty]
-    @morality = data[:Morality]
-    @anger = data[:Anger]
-    @anxiety = data[:Anxiety]
-    @melancholy = data[:Melancholy]
+    @adventurousness = data["Adventurousness"]
+    @artistic = data["Artistic interests"]
+    @emotionality = data["Emotionality"]
+    @imagination = data["Imagination"]
+    @authority_challenging = data["Authority-challenging"]
+    @cautiousness = data["Achievement striving"]
+    @self_discipline = data["Self-discipline"]
+    @activity_level = data["Activity level"]
+    @assertiveness = data["Assertiveness"]
+    @cheerfulness = data["Cheerfulness"]
+    @excitement_seeking = data["Excitement-seeking"]
+    @friendliness = data["Outgoing"]
+    @altruism = data["Altruism"]
+    @modesty = data["Modesty"]
+    @morality = data["Morality"]
+    @anger = data["Anger"]
+    @anxiety = data["Anxiety"]
+    @melancholy = data["Melancholy"]
   end
 
   def self.generate_from_watson(text)
-    raw_data = WatsonService.new(text).parse_traits.symbolize_keys
+    raw_data = WatsonService.new(text).parse_traits
     Personality.new(raw_data)
   end
 end
