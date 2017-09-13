3960b9005aafef43ad464b11c91f56f66ba4dc2f
Ellen Cooper <ellencooper74@gmail.com>
2017-09-13 07:33:47 -0600

    Configures Circliful

---
+++ app/assets/javascripts/circle_graphs.js
@@ -0,0 +1,11 @@
+
+
+
+$( document ).ready(function() {
+$("#your-circle").circliful({
+            animationStep: 5,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 15,
+            percent: 75
+       });
+});
