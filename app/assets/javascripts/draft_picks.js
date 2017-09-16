5b1adcba0ad431566f4f557d0367364c4e0fc3b3
Ellen Cooper <ellencooper74@gmail.com>
2017-09-15 23:27:37 -0600

    adds data-pos and data-id to contestant cards

--- app/assets/javascripts/draft_picks.js
+++ app/assets/javascripts/draft_picks.js
@@ -1,5 +1,17 @@
+var ready, set_positions;
+
+set_positions = function(){
+    // loop through and give each task a data-pos
+    // attribute that holds its position in the DOM
+    $('.card.draft-card').each(function(i){
+        $(this).attr("data-pos",i+1);
+    });
+}
+
+
 var ready;
 ready = function(){
+    set_positions();
     // call sortable on our div with the sortable class
     sortable('.sortable');
 }
