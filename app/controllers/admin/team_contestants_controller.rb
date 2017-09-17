51abdbbfca006f5f8c8998d90e10c383d56a42a3
Ellen Cooper <ellencooper74@gmail.com>
2017-09-17 12:37:45 -0600

    completes initiate_draft method

--- app/controllers/admin/team_contestants_controller.rb
+++ app/controllers/admin/team_contestants_controller.rb
@@ -1,6 +1,6 @@
 class Admin::TeamContestantsController < Admin::BaseController
   def create
-    TeamContestant.draft
+    TeamContestant.initiate_draft(current_season)
     redirect_to admin_dashboard_path
   end
 end
--- app/models/team_contestant.rb
+++ app/models/team_contestant.rb
@@ -1,4 +1,22 @@
 class TeamContestant < ApplicationRecord
   belongs_to :contestant
   belongs_to :team
+
+  def self.initiate_draft(season)
+    season.leagues.each do |league|
+      taken = []
+      until taken.count == season.contestants.count do
+        league.teams.each do |team|
+            team.draft_picks.order(:rank).each do |pick|
+              if !taken.include?(pick.contestant)
+                team.contestants << pick.contestant
+                taken << pick.contestant
+                break
+              end
+            end
+          end
+      end
+    end
+  end
+
 end
