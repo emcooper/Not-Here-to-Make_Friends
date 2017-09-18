fd1a409e7e084b3311891a11d6a65e8441260f0a
Ellen Cooper <ellencooper74@gmail.com>
2017-09-18 08:34:50 -0600

    creates league#team_points RESTful endpoint

--- app/models/league.rb
+++ app/models/league.rb
@@ -11,4 +11,11 @@ class League < ApplicationRecord
   def owner_of(contestant)
     teams.joins(:contestants).find_by("contestants.id = ?", contestant.id)
   end
+
+  def team_points
+    teams.map do |team|
+      points = season.weeks.order(:week_number).map {|week| team.week_points(week)}
+      {name: team.name, points: points}
+    end
+  end
 end
