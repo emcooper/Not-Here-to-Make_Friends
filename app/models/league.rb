703da462a03443c7f4a1d7dcf4bd8d027eacb915
Ellen Cooper <ellencooper74@gmail.com>
2017-09-18 01:11:56 -0600

    adds owner_of method to league; adds team owner of mvp to league show

--- app/models/league.rb
+++ app/models/league.rb
@@ -7,4 +7,8 @@ class League < ApplicationRecord
           .joins(:team_members)
           .find_by("team_members.user_id = ?", user.id)
   end
+
+  def owner_of(contestant)
+    teams.joins(:contestants).find_by("contestants.id = ?", contestant.id)
+  end
 end
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -13,5 +13,14 @@ RSpec.describe League, type: :model do
       user_team = Team.first
       expect(active_league.team_of_user(user)).to eq(user_team)
     end
+
+    it "#owner_of returns the team that owns the contestant" do
+      user = create(:user, :full_setup)
+      league_1 = League.first
+      contestant_1 = Contestant.first
+      team_1 = Team.first
+
+      expect(league_1.owner_of(contestant_1)).to eq(team_1)
+    end
   end
 end
