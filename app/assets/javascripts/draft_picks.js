dcd79ae4ba17215f0f4a350f80d674004e9e7e90
Ellen Cooper <ellencooper74@gmail.com>
2017-11-07 17:34:29 -0700

    refactors drag and drop functionality

--- app/assets/javascripts/draft_picks.js
+++ app/assets/javascripts/draft_picks.js
@@ -1,17 +1,16 @@
-$(document).ready(ready)
+$(document).ready(setDraftPicks)
 
-function ready(){
-  if($('div').is('.sortable')){
-    sortable('.sortable')[0].addEventListener('sortstop', function() {
-      updated_order = []
-      $('.card.draft-card').each(function(i){
-          updated_order.push({ id: $(this).data('id'), position: i+1 })
-        })
-        $.ajax({
-            type: "PUT",
-            url: `/teams/${$('.team_information').data('id')}/draft_picks`,
-            data: { order: updated_order }
+function setDraftPicks(){
+  sortable('.sortable')[0].addEventListener('sortstop', function() {
+    let updated_order = []
+    $('.card.draft-card').each(function(i){
+        updated_order.push({ id: $(this).data('id'), position: i+1 })
       })
+
+      $.ajax({
+          type: "PUT",
+          url: `/teams/${$('.team_information').data('id')}/draft_picks`,
+          data: { order: updated_order }
     })
-  }
+  })
 }
