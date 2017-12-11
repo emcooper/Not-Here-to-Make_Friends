4a3eb76561e40c1313d70c7e931acb513d82fd8f
Ellen Cooper <ellencooper74@gmail.com>
2017-12-10 20:51:36 -0700

    removes redundant personality related tests

--- spec/services/watson_service_spec.rb
+++ spec/services/watson_service_spec.rb
@@ -3,7 +3,7 @@ describe WatsonService do
     VCR.use_cassette("services/watson/personality_profile") do
       contestant = create(:contestant, twitter_handle: "@TheAbDoctor")
       tweets = TwitterService.new(contestant).tweets
-      @watson_service = WatsonService.new(tweets)
+      @watson_service = WatsonService.new(tweets, contestant)
       profile = @watson_service.personality_profile[:personality]
 
       expect(profile).to be_a(Array)
@@ -12,22 +12,6 @@ describe WatsonService do
       expect(profile.first[:percentile]).to be_a(Float)
       expect(profile.first[:children].first[:name]).to be_a(String)
       expect(profile.first[:children].first[:percentile]).to be_a(Float)
-    end
-  end
-
-  it "parse_traits returns hash of trait names and percentages" do
-    VCR.use_cassette("services/watson/parse_traits") do
-      contestant = create(:contestant, twitter_handle: "@TheAbDoctor")
-      tweets = TwitterService.new(contestant).tweets
-      @watson_service = WatsonService.new(tweets)
-
-      traits = @watson_service.parse_traits
-
-      expect(traits.count).to eq(30)
-      expect(traits.keys.first).to be_a(String)
-      expect(traits.values.first).to be_a(Float)
-      expect(traits.keys.last).to be_a(String)
-      expect(traits.values.last).to be_a(Float)
     end
   end
 end
