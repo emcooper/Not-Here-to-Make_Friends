5e2e4f01705b0ee8142784753ee9b1b82fda2225
Ellen Cooper <ellencooper74@gmail.com>
2017-12-17 12:18:15 -0700

    omniauth implemented (this setup has worked at least once loggin in and out)

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,5 +1,14 @@
 class Identity < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: :true
+  validates_presence_of :uid, :provider
 
-  validates_presence_of :user_id, :uid, :provider
+  def self.find_with_omniauth(auth)
+    find_by(uid: auth['uid'], provider: auth['provider'])
+  end
+
+  def self.create_with_omniauth(auth)
+    user = User.find_by(email: auth[:info][:email])
+    create(uid: auth['uid'], provider: auth['provider'], user: user)
+  end
+
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'], scope: ‘public_profile’, info_fields: ‘id,name,link’
+  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
+  provider_ignores_state: true
 end
