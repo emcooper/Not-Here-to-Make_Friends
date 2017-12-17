7bbc3e332815b8e8d18d9769210cb1230dc937fa
Ellen Cooper <ellencooper74@gmail.com>
2017-12-16 19:20:04 -0700

    adds table for league teams and join option

--- app/controllers/league_search_controller.rb
+++ app/controllers/league_search_controller.rb
@@ -1,8 +1,11 @@
 class LeagueSearchController < ApplicationController
   def show
     @league = League.find_by(name: params[:q])
-    redirect_to @league if @league
-    flash[:notice] = "That league doesn't exist! Make sure the name is exact."
-    redirect_to '/start'
+    if @league
+      redirect_to @league 
+    else
+      flash[:notice] = "That league doesn't exist! Make sure the name is exact."
+      redirect_to '/start'
+    end
   end
 end
