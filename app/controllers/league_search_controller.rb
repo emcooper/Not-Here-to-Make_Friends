dba309186213a0f3ad95f1879903db15a94c5222
Ellen Cooper <ellencooper74@gmail.com>
2017-12-16 18:39:05 -0700

    adds redirect for league search when league not found

--- app/controllers/league_search_controller.rb
+++ app/controllers/league_search_controller.rb
@@ -1,6 +1,8 @@
 class LeagueSearchController < ApplicationController
   def show
     @league = League.find_by(name: params[:q])
-    redirect_to @league
+    redirect_to @league if @league
+    flash[:notice] = "That league doesn't exist! Make sure the name is exact."
+    redirect_to '/start'
   end
 end
