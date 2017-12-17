645b28cf6c59bf86bd89f65e7e1d4da57c9c26d3
Ellen Cooper <ellencooper74@gmail.com>
2017-12-16 20:17:06 -0700

    adds redirects and flash messages

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -3,5 +3,6 @@ class TeamMembersController < ApplicationController
     team = Team.find(params[:team_id])
     TeamMember.create(team_id: team.id, user_id: params[:user_id])
     flash[:notice] = "You've joined team #{team.name}!"
+    redirect_to team.league
   end
 end
