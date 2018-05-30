98df616f70f7cf139cf4e66551ea79ea5d80845b
Ellen Cooper <ellencooper74@gmail.com>
2018-05-29 19:47:15 -0600

    adds current season to league search

--- app/controllers/league_search_controller.rb
+++ app/controllers/league_search_controller.rb
@@ -1,8 +1,8 @@
 class LeagueSearchController < ApplicationController
   def show
-    @league = League.find_by(name: params[:q])
+    @league = League.find_by(name: params[:q], season: current_season)
     if @league
-      redirect_to @league 
+      redirect_to @league
     else
       flash[:notice] = "That league doesn't exist! Make sure the name is exact."
       redirect_to '/start'
